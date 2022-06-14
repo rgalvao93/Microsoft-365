d4e9e88efe668b27a7d8d7cb1bea8b1099fc25ec
Rodrigo Galvão Pedro Pereira <37681316+rgalvao93@users.noreply.github.com>
2022-06-14 13:53:12 -0300

    update

--- PowerShell/Exchange Online/Recuperar mensagens excluídas na caixa de correio de um usuário Exchange Online.ps1
+++ PowerShell/Exchange Online/Recuperar mensagens excluídas na caixa de correio de um usuário Exchange Online.ps1
@@ -2,4 +2,4 @@
 Install-Module -Name ExchangeOnlineManagement
 Import-Module ExchangeOnlineManagement
 Connect-ExchangeOnline
-Restore-RecoverableItems -Identity "rodrigo.galvao@M365B882413.onmicrosoft.com","ronaldo@M365B882413.onmicrosoft.com" -FilterItemType IPM.Note -SubjectContains "emails" -FilterStartTime "3/15/2019 12:00:00 AM" -FilterEndTime "3/25/2019 11:59:59 PM" -MaxParallelSize 2
+Restore-RecoverableItems -Identity "email@dominio.com","email2@dominio.com" -FilterItemType IPM.Note -SubjectContains "emails" -FilterStartTime "3/15/2019 12:00:00 AM" -FilterEndTime "3/25/2019 11:59:59 PM" -MaxParallelSize 2
