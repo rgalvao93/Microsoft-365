dc27326dcf19e29204d3609e6a91732e27e284da
Rodrigo Galvão Pedro Pereira <37681316+rgalvao93@users.noreply.github.com>
2022-06-14 13:51:32 -0300

    update

--- PowerShell/Exchange Online/Corrigir lentidão no e-mail.ps1
+++ PowerShell/Exchange Online/Corrigir lentidão no e-mail.ps1
@@ -1,9 +1,9 @@
 ﻿Set-executionpolicy unrestricted -Force
 Install-Module ExchangeOnlineManagement -Force
 Connect-ExchangeOnline
-Remove-MailboxUserConfiguration -Mailbox andre@tecbis.com.br -Identity Configuration\IPM.Configuration.Suite.Storage -Confirm:$false
-Remove-MailboxUserConfiguration -Mailbox andre@tecbis.com.br -Identity Configuration\IPM.Configuration.Agregated.OWAUserConfiguration -Confirm:$false
-Remove-MailboxUserConfiguration -Mailbox andre@tecbis.com.br -Identity Configuration\IPM.Configuration.OWA.AutocompleteCache -Confirm:$false
-Remove-MailboxUserConfiguration -Mailbox andre@tecbis.com.br -Identity Configuration\IPM.Configuration.OWA.SessionInformation -Confirm:$false
-Remove-MailboxUserConfiguration -Mailbox andre@tecbis.com.br -Identity Configuration\IPM.Configuration.OWA.UserOptions -Confirm:$false
-Remove-MailboxUserConfiguration -Mailbox andre@tecbis.com.br -Identity Configuration\IPM.Configuration.OWA.ViewStateConfiguration -Confirm:$false
+Remove-MailboxUserConfiguration -Mailbox email@dominio.com -Identity Configuration\IPM.Configuration.Suite.Storage -Confirm:$false
+Remove-MailboxUserConfiguration -Mailbox email@dominio.com -Identity Configuration\IPM.Configuration.Agregated.OWAUserConfiguration -Confirm:$false
+Remove-MailboxUserConfiguration -Mailbox email@dominio.com -Identity Configuration\IPM.Configuration.OWA.AutocompleteCache -Confirm:$false
+Remove-MailboxUserConfiguration -Mailbox email@dominio.com -Identity Configuration\IPM.Configuration.OWA.SessionInformation -Confirm:$false
+Remove-MailboxUserConfiguration -Mailbox email@dominio.com -Identity Configuration\IPM.Configuration.OWA.UserOptions -Confirm:$false
+Remove-MailboxUserConfiguration -Mailbox email@dominio.com -Identity Configuration\IPM.Configuration.OWA.ViewStateConfiguration -Confirm:$false
