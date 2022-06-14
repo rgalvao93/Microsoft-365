eaa1d8653fd0f8b40b90ff6043967d7692745489
Rodrigo Galvão Pedro Pereira <37681316+rgalvao93@users.noreply.github.com>
2022-06-14 13:52:35 -0300

    update

--- PowerShell/Exchange Online/Inserir mensagem de resposta automatica em um e-mail durante um período.ps1
+++ PowerShell/Exchange Online/Inserir mensagem de resposta automatica em um e-mail durante um período.ps1
@@ -1,1 +1,1 @@
-﻿Set-MailboxAutoReplyConfiguration -Identity flavio.macedo@M365B882413.onmicrosoft.com -AutoReplyState scheduled -StartTime "05/27/2022 00:00:01" -EndTime "12/26/2022 00:00:01" -InternalMessage "FRANGUINHO COM FAROFA!!!"
+﻿Set-MailboxAutoReplyConfiguration -Identity email@dominio.com -AutoReplyState scheduled -StartTime "05/27/2022 00:00:01" -EndTime "12/26/2022 00:00:01" -InternalMessage "FRANGUINHO COM FAROFA!!!"
