f6bffef86aea9ce122898447f26e44ac4f710e14
Rodrigo Galvão Pedro Pereira <rgalvao93@outlook.com>
2022-06-15 20:25:19 -0300

    update

--- Microsoft 365/Atribuir Licenças.ps1
+++ Microsoft 365/Atribuir Licenças.ps1
@@ -1,8 +1,8 @@
 ﻿Install-Module MSOnline
 Connect-MsolService
-#se o campo -UsageLocation não estiver preenchido, a licença não é atribuída
 Set-MsolUserLicense -UserPrincipalName usuario.teste@dominio.com -AddLicenses "NomeDaLicença"
-#atribuir o a região do cliente
+    #se o campo -UsageLocation não estiver preenchido, a licença não é atribuída
 Set-MsolUser -UserPrincipalName usuario.teste@dominio.com -UsageLocation BR
-#consultar se o atributo foi preenchido
+    #atribuir o a região do cliente
 Get-MsolUser -SearchString usuario.teste@dominio.com | fl
+    #consultar se o atributo foi preenchido
